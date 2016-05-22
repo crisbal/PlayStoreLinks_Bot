bc4ef6a8048c5291d332c07a350bf2623a62d4bd
Cristian Baldi <bld.cris.96@gmail.com>
2016-05-22 16:37:29 +0200

    Update tests

--- Test.py
+++ Test.py
@@ -3,26 +3,24 @@ import Config
 import logging
 Config.loggingLevel = logging.ERROR
 
-import LinkMeBot as linkMe
+from PlayStore import PlayStore
 
 
 class TestLinkMe(unittest.TestCase):
     def setUp(self):
-        self.appPlayStore = linkMe.searchOnPlayStore("facebook")
-        self.appPlayStore2 = linkMe.searchOnPlayStore("Sleep as Android Unlock")
+        self.appPlayStore = PlayStore.search("facebook")
+        self.appPlayStore2 = PlayStore.search("Sleep as Android Unlock")
         
         #self.appDB = linkMe.searchInDatabase("Facebook")
 
     def test_playStore(self):
-        self.assertEqual(self.appPlayStore.fullName, "Facebook",'Wrong fullName')
+        self.assertEqual(self.appPlayStore.name, "Facebook",'Wrong name')
         self.assertEqual(self.appPlayStore.free, True,'Wrong Price')
         self.assertEqual(self.appPlayStore.link, "https://play.google.com/store/apps/details?id=com.facebook.katana",'Wrong Link')
-        self.assertEqual(self.appPlayStore.searchName, "facebook",'Wrong Search Name')
 
-        self.assertEqual(self.appPlayStore2.fullName, "Sleep as Android Unlock",'Wrong fullName')
+        self.assertEqual(self.appPlayStore2.name, "Sleep as Android Unlock",'Wrong fullName')
         self.assertEqual(self.appPlayStore2.free, False,'Wrong Price')
         self.assertEqual(self.appPlayStore2.link, "https://play.google.com/store/apps/details?id=com.urbandroid.sleep.full.key",'Wrong Link')
-        self.assertEqual(self.appPlayStore2.searchName, "Sleep as Android Unlock",'Wrong Search Name')
 
 
 if __name__ == '__main__':
