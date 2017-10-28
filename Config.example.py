1315eaf44657fae398f6e7ec467dc0fcd2639954
Cristian Baldi <bld.cris.96@gmail.com>
2017-10-28 19:55:53 +0200

    Swap Config with an example file

---
+++ Config.example.py
@@ -0,0 +1,21 @@
+import logging
+
+#Login Info
+username = "username"
+password = "password"
+
+#LOGGING
+loggingLevel = logging.DEBUG
+loggerName = "LinkMeBot"
+
+#FILES
+logFile = "bot.log"
+logFileDelete = "botDelete.log"
+botRunningFile = "/tmp/botRunning"
+
+#SUBREDDITS
+subreddits = ["cris9696"]
+
+#REPLY
+maxAppsPerComment = 10
+closingFormula = "\n\n------\n\n[^Source ^Code](https://github.com/crisbal/PlayStoreLinks_Bot) ^| [^Feedback/Bug ^report](http://www.reddit.com/r/cris9696) ^| ^Bot ^by ^/u/cris9696 "
